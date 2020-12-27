72694017f30da335ba95ff4908aa3ac46f5d2a8e
ehvenimunis <muhammedsalihaydogan@gmail.com>
2020-12-27 10:58:32 +0300

    sec

---
+++ Competitive Programming with STL/std::sort() in C++ STL/Non-Manipulating Algorithms/sort_and_reverse.cpp
@@ -0,0 +1,60 @@
+// A C++ program to demonstrate working of sort(),
+// reverse()
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <numeric> //For accumulate operation
+using namespace std;
+
+int main()
+{
+	// Initializing vector with array values
+	int arr[] = {10, 20, 5, 23 ,42 , 15};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	vector<int> vect(arr, arr+n);
+
+	cout << "Vector is: ";
+	for (int i=0; i<n; i++)
+		cout << vect[i] << " ";
+
+	// Sorting the Vector in Ascending order
+	sort(vect.begin(), vect.end());
+
+	cout << "\nVector after sorting is: ";
+	for (int i=0; i<n; i++)
+	cout << vect[i] << " ";
+
+	// Reversing the Vector
+	reverse(vect.begin(), vect.end());
+
+	cout << "\nVector after reversing is: ";
+	for (int i=0; i<6; i++)
+		cout << vect[i] << " ";
+
+	cout << "\nMaximum element of vector is: ";
+	cout << *max_element(vect.begin(), vect.end());
+
+	cout << "\nMinimum element of vector is: ";
+	cout << *min_element(vect.begin(), vect.end());
+
+	// Starting the summation from 0
+	cout << "\nThe summation of vector elements is: ";
+	cout << accumulate(vect.begin(), vect.end(), 0);
+
+	return 0;
+}
+
+
+
+/*
+
+Output
+
+Vector is: 10 20 5 23 42 15 
+Vector after sorting is: 5 10 15 20 23 42 
+Vector after reversing is: 42 23 20 15 10 5 
+Maximum element of vector is: 42
+Minimum element of vector is: 5
+The summation of vector elements is: 115
+
+*/
